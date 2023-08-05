2fadbcb98731f33abad2e8f6ab78e158e6cbd587
hamsaebrahim <hamsaebrahim8@gmail.com>
2023-08-05 14:22:55 +0300

    nowrap

--- resources/views/layouts/partials/cart-header.blade.php
+++ resources/views/layouts/partials/cart-header.blade.php
@@ -1,25 +1,25 @@
-<div class="w-full flex flex-row bg-no-repeat bg-center text-center items-center"
+<div class="flex flex-row items-center w-full text-center bg-center bg-no-repeat"
     style="height: 200px; background-image: url('@if(!empty(session('_background_image'))){{ images_asset(asset('uploads/' . session('breadcrumb_background_image'))) }}@else{{ asset('images/default--bg.png') }}@endif')">
      {{--<div class="w-full">
         <div class="flex flex-row">
-            <div class="flex-3 text-white text-4xl font-bold text-left">
+            <div class="text-4xl font-bold text-left text-white flex-3">
                 @include('layouts.partials.language_dropdown')
             </div>
             <div class="flex-1 mt-8 text-center">
-                <p class="font-bold text-4xl text-white">@lang('lang.cart')</p>
+                <p class="text-4xl font-bold text-white">@lang('lang.cart')</p>
             </div>
-            <div class="flex-3 w-48 items-end">
+            <div class="items-end w-48 flex-3">
 
             </div>
         </div>
 
         <div class="container mx-auto mt-16">
             <div class="flex justify-end w-full">
-                <a class="text-xl text-white font-semibold px-2"
+                <a class="px-2 text-xl font-semibold text-white"
                     href="{{ action('HomeController@index') }}">@lang('lang.home')</a> <span
-                    class="text-xl text-white font-semibold px-1">-</span>
+                    class="px-1 text-xl font-semibold text-white">-</span>
                 @if (request()->segment(2) == 'cart')
-                    <a class="text-xl text-white font-semibold px-2"
+                    <a class="px-2 text-xl font-semibold text-white"
                         href="{{ action('CartController@view') }}">@lang('lang.cart')</a>
                 @endif
 
@@ -32,20 +32,20 @@
 
 
 
-<div class="container-fluid mx-auto">
+<div class="mx-auto container-fluid">
     <div class="flex flex-row subMenu">
 
-        <div class="flex-3 text-white text-4xl font-bold text-left w-48">
+        <div class="w-48 text-4xl font-bold text-left text-white flex-3">
             @include('layouts.partials.language_dropdown')
         </div>
         <div class="flex-1 ">
 
         </div>
 
-        <div class="flex-3 text-white  font-bold text-right w-48">
+        <div class="w-48 font-bold text-right text-white flex-3">
                 <div class="dropdown">
                     <a href="{{ action('HomeController@index') }}"
-                    class="dropbtn bg-red text-white md:text-base xs:text-sm font-bold px-4 py-2 border-2 border-white rounded-lg">
+                    class="px-4 py-2 font-bold text-white border-2 border-white rounded-lg dropbtn bg-red md:text-base xs:text-sm" style="white-space:nowrap;" >
                         @lang('lang.home') <i class="fas fa-caret-down"></i></a>
                     <div class="dropdown-content">
                         <a href="{{ action('AboutUsController@index') }}"> @lang('lang.about_us')</a>
--- resources/views/layouts/partials/category-header.blade.php
+++ resources/views/layouts/partials/category-header.blade.php
@@ -1,15 +1,15 @@
-<div class="w-full flex flex-row bg-no-repeat bg-center text-center items-center "
+<div class="flex flex-row items-center w-full text-center bg-center bg-no-repeat "
     style="background-size:100% 100%;height: 200px; background-image: url('@if(!empty(session('breadcrumb_background_image'))){{ images_asset(asset('uploads/' . session('breadcrumb_background_image'))) }}@else{{ asset('images/default-breadcrumb-bg.png') }}@endif');">
     <div class="w-full">
- 
+
 
         <div class="container mx-auto mt-16">
             {{--<div class="flex justify-end w-full">
-                <a class="md:text-xl xs:text-sm text-white font-semibold px-2 text-white-shadow "
+                <a class="px-2 font-semibold text-white md:text-xl xs:text-sm text-white-shadow "
                     href="{{ action('HomeController@index') }}">@lang('lang.home')</a> <span
-                    class="md:text-xl xs:text-sm text-white font-semibold px-1">-</span>
-                <a class="md:text-xl xs:text-sm text-white font-semibold px-2 text-white-shadow"
-                    href="{{ action('ProductController@getProductListByCategory', $category->id) }}">{{ $category->name }}</a> 
+                    class="px-1 font-semibold text-white md:text-xl xs:text-sm">-</span>
+                <a class="px-2 font-semibold text-white md:text-xl xs:text-sm text-white-shadow"
+                    href="{{ action('ProductController@getProductListByCategory', $category->id) }}">{{ $category->name }}</a>
             </div>--}}
         </div>
     </div>
@@ -19,15 +19,15 @@
     use App\Models\ProductClass;
     $categories = ProductClass::orderBy('sort', 'asc')->where('status', 1)->where('name', '!=', 'Extras')->get();
 @endphp
-<div class="container-fluid mx-auto">
+<div class="mx-auto container-fluid">
     <div class="flex flex-row">
-        <div class="flex-3 text-white text-4xl font-bold text-left w-48">
+        <div class="w-48 text-4xl font-bold text-left text-white flex-3">
             @include('layouts.partials.language_dropdown')
         </div>
         <div class="flex-1 ">
-            <div class="width-fit h-10 bg-red text-white mx-auto text-center   rounded-xl ">
+            <div class="h-10 mx-auto text-center text-white width-fit bg-red rounded-xl ">
                 <div class="dropdown">
-                    <button class="dropbtn text-2xl text-white font-semibold py-1">{{ $category->name }}  <i class="fas fa-caret-down"></i> </button>
+                    <button class="py-1 text-2xl font-semibold text-white dropbtn">{{ $category->name }}  <i class="fas fa-caret-down"></i> </button>
                     <div class="dropdown-content">
                         <a href="#"> @lang('lang.categories')</a>
                         @foreach ($categories as $category)
@@ -37,10 +37,10 @@
                   </div>
             </div>
         </div>
-        <div class="flex-3 text-white  font-bold text-right w-48">
+        <div class="w-48 font-bold text-right text-white flex-3">
             <div class="dropdown">
-                <a href="{{ action('HomeController@index') }}" 
-                class="dropbtn bg-red text-white md:text-base xs:text-sm font-bold px-4 py-2 border-2 border-white rounded-lg">
+                <a href="{{ action('HomeController@index') }}"
+                class="px-4 py-2 font-bold text-white border-2 border-white rounded-lg dropbtn bg-red md:text-base xs:text-sm"  style="white-space:nowrap;" >
                     @lang('lang.home') <i class="fas fa-caret-down"></i></a>
                 <div class="dropdown-content">
                     <a href="{{ action('AboutUsController@index') }}"> @lang('lang.about_us')</a>
